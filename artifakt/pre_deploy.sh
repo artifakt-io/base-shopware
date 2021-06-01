ee0ddabe1545ef87bf7d1d7ed0bf62b86c1628f6
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-06-01 21:23:45 +0200

    Edit deploy.sh script

--- artifakt/pre_deploy.sh
+++ artifakt/pre_deploy.sh
@@ -2,7 +2,9 @@
 
  if [[ $CLEAR_DATABASE -eq 1 ]]; then
     echo "Removing all tables"
-    mysql -u $ARTIFAKT_MYSQL_USER -h $ARTIFAKT_MYSQL_HOST $ARTIFAKT_MYSQL_DATABASE_NAME -p$MYSQL_PASSWORD < artifakt/clearTables.sql
+    ( mysqldump --add-drop-table --no-data -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE_NAME -h 127.0.0.1 | grep 'DROP TABLE' ) > ./drop_all_tables.sql
+    mysql -u $MYSQL_USER -h 127.0.0.1 -p$MYSQL_PASSWORD $MYSQL_DATABASE_NAME < ./drop_all_tables.sql
+    rm ./drop_all_tables.sql
  fi
 
 composer install
