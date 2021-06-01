151429e1ea5b8d8567876f805ce49328c6c6f0f9
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-06-01 16:22:42 +0200

    Edit pre_deploy.sh script

---
+++ artifakt/pre_deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ if [[ $CLEAR_DATABASE -eq 1 ]]; then
+    echo "Removing all tables"
+    mysql -u $ARTIFAKT_MYSQL_USER -h $ARTIFAKT_MYSQL_HOST $ARTIFAKT_MYSQL_DATABASE_NAME -p$MYSQL_PASSWORD < artifakt/clearTables.sql
+ fi
