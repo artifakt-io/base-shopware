884499c81843652283f59cc741ca3bd1d27131c8
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-06-03 19:19:50 +0200

    Edit pre_deploy script

--- artifakt/pre_deploy.sh
+++ artifakt/pre_deploy.sh
@@ -22,7 +22,7 @@ if [[ $ARTIFAKT_IS_MAIN_INSTANCE -eq 1 ]]; then
    if [[ $CLEAR_DATABASE -eq 1 ]]; then
       echo "Removing all tables"
       echo "set foreign_key_checks=0;" > ./drop_all_tables.sql
-      ( mysqldump --add-drop-table --no-data -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE_NAME -h 127.0.0.1 | grep 'DROP TABLE' ) >> ./drop_all_tables.sql
+      ( mysqldump --add-drop-table --no-data -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE_NAME -h $MYSQL_HOST | grep 'DROP TABLE' ) >> ./drop_all_tables.sql
       mysql -u $MYSQL_USER -h 127.0.0.1 -p$MYSQL_PASSWORD $MYSQL_DATABASE_NAME < ./drop_all_tables.sql
       rm ./drop_all_tables.sql
       
