d0c8f09935313d41ec472cddd03aeb6a729d9833
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-08-26 16:17:42 +0200

    Add Storefront name to env variables and edit on-install script to use it

--- .artifakt/shopware/rootfs/etc/shopware/scripts/on-init/config-fixer.sh
+++ .artifakt/shopware/rootfs/etc/shopware/scripts/on-init/config-fixer.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-rm /var/www/html/config/services/defaults_test.xml
+if [ -f /var/www/html/config/services/defaults_test.xml ]; then
+    rm /var/www/html/config/services/defaults_test.xml
+fi
+
 cp /tmp/rootfs/etc/shopware/configs/services/jwt.xml /var/www/html/config/services/
 cp /tmp/rootfs/etc/shopware/configs/services/services.xml /var/www/html/config/
--- .artifakt/shopware/rootfs/etc/shopware/scripts/on-install/01-install-sw.sh
+++ .artifakt/shopware/rootfs/etc/shopware/scripts/on-install/01-install-sw.sh
@@ -7,6 +7,6 @@ if [ ! -f /var/www/html/config/jwt/private.pem ]; then
 fi
 su -s /bin/sh -c 'php /tmp/rootfs/fix-install.php' www-data
 su -s /bin/sh -c 'php /var/www/html/bin/console user:create "$INSTALL_ADMIN_USERNAME" --admin --password="$INSTALL_ADMIN_PASSWORD" -n' www-data
-su -s /bin/sh -c 'php /var/www/html/bin/console sales-channel:create:storefront --name=Storefront --url="$APP_URL"' www-data
-su -s /bin/sh -c 'php /var/www/html/bin/console theme:change --all Storefront' www-data
+su -s /bin/sh -c 'php /var/www/html/bin/console sales-channel:create:storefront --name=$INSTALL_STOREFRONT_NAME --url="$APP_URL"' www-data
+su -s /bin/sh -c 'php /var/www/html/bin/console theme:change --all $INSTALL_STOREFRONT_NAME' www-data
 cp /shopware_version /state/installed_version
