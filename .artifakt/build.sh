17a29b6b94d103e6bb610727b127d8dbbda55ef2
djalal <hi@djal.al>
2021-09-28 14:49:58 +0200

    TIL: RUN --mount is readonly

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -11,15 +11,12 @@ export APP_DEBUG=1
 # see https://stackoverflow.com/a/61349991/1093649
 composer install --no-cache --optimize-autoloader --no-interaction --no-ansi --no-scripts
 
-#echo "export APP_ENV=$APP_ENV" >> /etc/apache2/envvars
-#echo "export APP_DEBUG=$APP_DEBUG" >> /etc/apache2/envvars
-
 mkdir /state && \
 	touch /var/www/html/install.lock && \
 	echo $SHOPWARE_VERSION > /shopware_version
 
 cp -rp /.artifakt/shopware/rootfs /tmp/
 
-chown -R www-data:www-data ./vendor ./install.lock /tmp
+chown -R www-data:www-data ./vendor ./install.lock /tmp/rootfs
 
 echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
